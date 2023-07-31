67e4c424464a7993e3a4729ed0a7c38e8e33151d
ShreyaSev <shreya21110440@snuchennai.edu.in>
2023-07-31 22:09:57 +0530

    finished

--- labex3/scripts/app.js
+++ labex3/scripts/app.js
@@ -4,12 +4,18 @@ angular.module('myApp',[])
 
     $scope.toggleMode = function(){
         $scope.isDarkMode = !$scope.isDarkMode;
-        css_link = document.getElementById("stylesheet")
+        css_link1 = document.getElementById("stylesheet")
+        // css_link2 =document.getElementById('stylesheet2')
+        iframe = document.getElementsByTagName('iframe')[0]
+        iframeDocument = iframe.contentDocument
+        css_link2 = iframeDocument.getElementById('stylesheet2')
         if ($scope.isDarkMode){
-            css_link.href = "styles/darkmode.css"
+            css_link1.href = "styles/darkmode.css"
+            css_link2.href = "styles/darkmode.css"
         }
         else {
-            css_link.href = "styles/lightmode.css"
+            css_link1.href = "styles/lightmode.css"
+            css_link2.href = "styles/lightmode.css"
         }
     }
 }
