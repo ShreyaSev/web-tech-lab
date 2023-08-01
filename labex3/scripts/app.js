5faec11e25fc62901c2bf64e1ce28765f02a6e5e
ShreyaSev <shreya21110440@snuchennai.edu.in>
2023-08-01 09:57:24 +0530

    removed iframe

--- labex3/scripts/app.js
+++ labex3/scripts/app.js
@@ -6,16 +6,16 @@ angular.module('myApp',[])
         $scope.isDarkMode = !$scope.isDarkMode;
         css_link1 = document.getElementById("stylesheet")
         // css_link2 =document.getElementById('stylesheet2')
-        iframe = document.getElementsByTagName('iframe')[0]
-        iframeDocument = iframe.contentDocument
-        css_link2 = iframeDocument.getElementById('stylesheet2')
+        // iframe = document.getElementsByTagName('iframe')[0]
+        // iframeDocument = iframe.contentDocument
+        // css_link2 = iframeDocument.getElementById('stylesheet2')
         if ($scope.isDarkMode){
             css_link1.href = "styles/darkmode.css"
-            css_link2.href = "styles/darkmode.css"
+            // css_link2.href = "styles/darkmode.css"
         }
         else {
             css_link1.href = "styles/lightmode.css"
-            css_link2.href = "styles/lightmode.css"
+            // css_link2.href = "styles/lightmode.css"
         }
     }
 }
